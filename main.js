4a1d6d543ec028b739a1ee038eb8508195bee544
Sam Foong <samantha.foong@atmotech.co.uk>
2022-04-24 23:23:17 +0100

    L10 completed

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        addCart(id) {
+            this.cart.push(id)
+            console.log(this.cart)
+        },
+        removeCart(id){
+            for(var i=0; i<this.cart.length; i++) {
+                if(this.cart[i]===id) {
+                    this.cart.splice(i,1);
+                }
+            }
+            console.log(this.cart)
+        },
+    }
 })
